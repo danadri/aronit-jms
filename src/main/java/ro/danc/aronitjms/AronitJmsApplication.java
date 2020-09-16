5fa7752a77b8bc8a276e560eec8b2cfa2f5e0048
danadri <danciucu@gmail.com>
2020-09-16 21:39:04 +0300

    Initialize project

---
+++ src/main/java/ro/danc/aronitjms/AronitJmsApplication.java
@@ -0,0 +1,13 @@
+package ro.danc.aronitjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AronitJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AronitJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/danc/aronitjms/AronitJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.danc.aronitjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AronitJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
